8b84cb5d2e499b75eb1225c93c5ad2cf384bdf8a
unknown <hamidabadani63@gmail.com>
2022-04-21 16:22:05 +0430

    js file and index.html changed

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+let result = document.getElementById("result")
+let btn = document.querySelectorAll('.btn')
+btn.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        display(btn.value)
+    })
+})
+
+
 // This function clear all the values
 function clearScreen() {
-    document.getElementById("result").value = "";
+    result.value = "";
 }
 
 
 // This function display values
 function display(value) {
-    document.getElementById("result").value += value;
+    if (value=='+' ||value=='-' || value=='*'||value=='/'){
+        if (result.value[result.value.length-1] == '+' ||
+            result.value[result.value.length-1] == '-' ||
+            result.value[result.value.length-1] == '*' ||
+            result.value[result.value.length-1] == '/' ){
+
+        } else {result.value += value;}
+    } else {
+
+        result.value += value;
+
+    }
 }
 
 // This function evaluates the expression and return result
 function calculate() {
-    var p = document.getElementById("result").value;
+    var p = result.value;
     var q = eval(p);
-    document.getElementById("result").value = q;
+    result.value = q;
 }
